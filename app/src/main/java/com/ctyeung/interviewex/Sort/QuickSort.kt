c0b27ae8b17bc8739c1d9a47d05a5879705ae079
Chi Yeung <yeun0007@umn.edu>
2020-05-04 11:25:53 -0500

    QuickSort
    implementation & junit

--- app/src/main/java/com/ctyeung/interviewex/Sort/QuickSort.kt
+++ app/src/main/java/com/ctyeung/interviewex/Sort/QuickSort.kt
@@ -1,4 +1,47 @@
 package com.ctyeung.interviewex.Sort
 
-class QuickSort {
+import java.util.Random
+import kotlin.random.Random.Default
+
+object QuickSort {
+
+    /*
+     * Any pivot is ok so long it does change so we can stuck in an infinite loop
+     * Random is not random, so use an incremental pivot
+     */
+    fun sort(list:ArrayList<Int>, pivot:Int=0):ArrayList<Int>{
+        if(list.size > 1) {
+            var listLeft = ArrayList<Int>()
+            var listRight = ArrayList<Int>()
+
+            // divide list
+            var p = pivot
+            if(p > list.size-1)
+                p = 0
+
+            for(num in list){
+                if(num < list[p]) {
+                    listLeft.add(num)
+                }
+                else {
+                    listRight.add(num)
+                }
+            }
+
+            // recursion
+            p++
+            if(listLeft.size>1)
+                listLeft = sort(listLeft, p)
+
+            if(listRight.size>1)
+                listRight = sort(listRight, p)
+
+            // merge lists
+            listLeft.addAll(listRight)
+            return listLeft
+        }
+        else {
+            return list
+        }
+    }
 }
---
+++ app/src/test/java/com/ctyeung/interviewex/Sort/QuickSortTest.kt
@@ -0,0 +1,33 @@
+package com.ctyeung.interviewex.Sort
+
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class QuickSortTest {
+    val TEST_NUM1 = 1
+    val TEST_NUM2 = 2
+    val TEST_NUM3 = 3
+    val TEST_NUM4 = 4
+    val TEST_NUM5 = 5
+    val TEST_NUM6 = 6
+
+    @Test
+    fun sort() {
+    var list = arrayListOf<Int>(  TEST_NUM3,
+                                    TEST_NUM1,
+                                    TEST_NUM6,
+                                    TEST_NUM4,
+                                    TEST_NUM2,
+                                    TEST_NUM5)
+
+        var sorted = QuickSort.sort(list)
+
+        assertEquals(TEST_NUM1, sorted[0])
+        assertEquals(TEST_NUM2, sorted[1])
+        assertEquals(TEST_NUM3, sorted[2])
+        assertEquals(TEST_NUM4, sorted[3])
+        assertEquals(TEST_NUM5, sorted[4])
+        assertEquals(TEST_NUM6, sorted[5])
+    }
+}
