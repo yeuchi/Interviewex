7e0cfa1be34cc10ab264d2f5ead4dc1ed4d50afe
Chi Yeung <yeun0007@umn.edu>
2020-05-04 10:24:58 -0500

    MergeSort
    implementation & junit

--- app/src/main/java/com/ctyeung/interviewex/Sort/MergeSort.kt
+++ app/src/main/java/com/ctyeung/interviewex/Sort/MergeSort.kt
@@ -4,7 +4,44 @@ import kotlin.Comparable
 
 object MergeSort {
 
-    fun sort(list:ArrayList<Int>) {
+    fun sort(list:ArrayList<Int>, depth:Int=0):ArrayList<Int> {
 
+        if(list.size > 1){
+            val mid = list.size / 2
+            var subListLeft= ArrayList<Int>(list.subList(0, mid))
+            var subListRight = ArrayList<Int>(list.subList(mid, list.size))
+
+            var sortedLeft = sort(subListLeft, depth)
+            var sortedRight = sort(subListRight, depth)
+            var l = 0
+            var r = 0
+
+            // sorted 2 list
+            var sorted = ArrayList<Int>()
+            while (l < sortedLeft.size && r < sortedRight.size) {
+                val left = sortedLeft[l]
+                val right = sortedRight[r]
+                if (left < right) {
+                    sorted.add(left)
+                    l++
+                } else {
+                    sorted.add(right)
+                    r++
+                }
+            }
+
+            if(l < sortedLeft.size) {
+                val remainLeft = sortedLeft.subList(l, subListLeft.size)
+                sorted.addAll(remainLeft)
+            }
+            else if (r < sortedRight.size) {
+                val remainRight = sortedRight.subList(r, subListRight.size)
+                sorted.addAll(remainRight)
+            }
+            return sorted
+        }
+        else {
+            return list
+        }
     }
 }
